35b98c4a812a3bd6475aab533b941f1805396d4f
Elias <elias@notebook.home>
2026-02-16 16:54:47 -0300

    small tweaks

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const CONFIG = {
         'section-1': {
             title: "Cocina",
             menuTitle: "Muebles de cocina",
-            description: "Mesas, Sillas",
+            description: "Mesas, Sillas.",
             menuOrder: 2,
             images: [
                 { id: 'Mesa-S1-019', src: 'img/section1/019.jpeg', imageLabel: 'Mesa: 1.20m x 0.90m x 0.85m' },
