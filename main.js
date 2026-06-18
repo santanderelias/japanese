28b5582ac4124a890fcb9e182b8654a0cb70ca99
Elias Santander <cazagb@gmail.com>
2026-06-18 13:30:17 +0000

    small tweaks

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function createGalleryCard(src, index) {
   body.className = 'card-body';
   const caption = document.createElement('p');
   caption.className = 'gallery-caption mb-0';
-  caption.textContent = `Click para ver imagen ${index + 1}`;
+  caption.textContent = ` ${index + 1}`;
   body.appendChild(caption);
 
   card.appendChild(image);
